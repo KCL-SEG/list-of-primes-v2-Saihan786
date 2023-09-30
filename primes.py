a46e22a6d8296effe43d1f7b22cf0161ba21968e
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-09-30 17:42:41 +0100

    Add functionality to primes() method. It now finds specified number of prime numbers and returns a list.

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
     list = []
+    current_prime = 2
+    counter = number_of_primes
+
+    while counter>0:
+        isPrime = True
+        for j in range(2,current_prime-1):
+            if (current_prime%j) == 0:
+                isPrime = False
+                break
+        if isPrime:
+            list.append(current_prime)
+            counter-=1
+        current_prime+=1
     return list
